ea3e3b56d6563867c3039043836033e220d8bcd1
ken2 <kennethjhonaycardo@gmail.com>
2024-09-25 09:33:51 +0800

    push

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -9,6 +9,7 @@ import { Router } from '@angular/router';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
+
 export class SidebarComponent implements OnInit {
   isSidebarMinimized: boolean = false;
   isStudent = this.API.getUserType() == '0';
